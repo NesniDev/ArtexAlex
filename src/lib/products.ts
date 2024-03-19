ccd0630509f1ca361421aaed7ae9c4b120572145
Nestnidev <neidernieto@unisangil.edu.co>
2024-03-19 09:35:54 -0500

    first Commit - page main

---
+++ src/lib/products.ts
@@ -0,0 +1,30 @@
+export const PRODUCTS = [
+    {
+        img: "/imgProducts/mesa madera-metal.avif",
+        name: "Neider Nieto",
+        materials: "Varilla, madera",
+        price: "500.000",
+        description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf "
+    },
+    {
+        img: "/imgProducts/mesa madera-metal.avif",
+        name: "Neider Nieto",
+        materials: "Varilla, madera",
+        price: "500.000",
+        description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf "
+    },
+    {
+        img: "/imgProducts/mesa madera-metal.avif",
+        name: "Neider Nieto",
+        materials: "Varilla, madera",
+        price: "500.000",
+        description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf "
+    },
+    {
+        img: "/imgProducts/mesa madera-metal.avif",
+        name: "Neider Nieto",
+        materials: "Varilla, madera",
+        price: "500.000",
+        description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf "
+    },
+]
---
+++ src/lib/testimonials.ts
@@ -0,0 +1,14 @@
+export const TESTIMONIALS = [
+    {
+        img: "/foto.jpg",
+        name: "Jaime Ortiz",
+        stars: "⭐⭐⭐⭐⭐",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi  quibusdam harum facilis dolor."
+    },
+    {
+        img: "/foto.jpg",
+        name: "Jaime Ortiz",
+        stars: "⭐⭐⭐⭐",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi  quibusdam harum facilis dolor."
+    }
+]
