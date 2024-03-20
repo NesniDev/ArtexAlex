43283f7d7d5c2af597f5f2921fb1cc1bc0b7ae54
Nestnidev <neidernieto@unisangil.edu.co>
2024-03-19 22:09:33 -0500

    fix content and styles

--- src/lib/testimonials.ts
+++ src/lib/testimonials.ts
@@ -1,14 +1,20 @@
 export const TESTIMONIALS = [
     {
         img: "/foto.jpg",
-        name: "Jaime Ortiz",
+        name: "Laura Sánchez",
         stars: "⭐⭐⭐⭐⭐",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi  quibusdam harum facilis dolor."
+        text: "Los productos de esta empresa son simplemente impresionantes. Cada pieza irradia calidad y atención al detalle.",
     },
     {
         img: "/foto.jpg",
-        name: "Jaime Ortiz",
+        name: "Jairo Martinez",
         stars: "⭐⭐⭐⭐",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi  quibusdam harum facilis dolor."
+        text: "¡Estoy enamorado de mis muebles nuevos! No solo son hermosos, sino que también puedo sentir la calidad en cada detalle.",
+    },
+    {
+        img: "/foto.jpg",
+        name: "Maria Diaz",
+        stars: "⭐⭐⭐⭐⭐",
+        text: "Como amante del arte, me emociona ver cómo estos productos transforman desechos en obras maestras.",
     }
 ]
