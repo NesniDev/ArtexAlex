52a6b00e229fd00f28f2ccb4758a1bb5f292e50d
Nestnidev <neidernieto@unisangil.edu.co>
2024-03-21 15:36:57 -0500

    page dynamic of the products

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -1,5 +1,6 @@
 export const PRODUCTS = [
     {
+        id: "Mesa-4-Puestos",
         img: "/imgProducts/mesa.avif",
         name: "Mesa 4 Puestos",
         materials: "Madera, discos bicicleta",
@@ -7,13 +8,16 @@ export const PRODUCTS = [
         description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf "
     },
     {
+        id: "Silla-Reclinable",
         img: "/imgProducts/silla.avif",
         name: "Silla Reinclinable",
         materials: "Varilla, madera",
         price: "500.000",
-        description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf "
+        description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf ",
+        descriptionComplete: "lore "
     },
     {
+        id: "Banca-2-llantas",
         img: "/imgProducts/banca.avif",
         name: "Banca",
         materials: "Llantas, Madera",
@@ -21,6 +25,7 @@ export const PRODUCTS = [
         description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf "
     },
     {
+        id: "Moto-cadena",
         img: "/imgProducts/moto.avif",
         name: "Moto",
         materials: "Cadena de cicla",
@@ -28,13 +33,15 @@ export const PRODUCTS = [
         description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf "
     },
     {
+        id: "Perro Alambre",
         img: "/imgProducts/perro.avif",
-        name: "Mesa 4 Puestos",
-        materials: "Madera, discos bicicleta",
+        name: "Perro alambre",
+        materials: "Metal, alambre",
         price: "500.000",
         description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf "
     },
     {
+        id: "Silla-Reclinable",
         img: "/imgProducts/silla.avif",
         name: "Silla Reinclinable",
         materials: "Varilla, madera",
