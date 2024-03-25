3953fc2914b7d8cff0b48016adfbb2ef8f600e73
Nestnidev <neidernieto@unisangil.edu.co>
2024-03-25 17:32:52 -0500

    Fix and modify styles

---
+++ src/lib/Allproducts.ts
@@ -0,0 +1,55 @@
+export const ALL_PRODUCTS = [
+    {
+        id: "Moto-cadena",
+        img: "/imgProducts/moto.avif",
+        name: "Moto",
+        materials: "Cadena de bicicleta",
+        price: "90.000",
+        description: "Este diseño único resalta tanto la creatividad como la funcionalidad, ofreciendo una alternativa innovadora en la construcción de vehículos."
+    },
+    
+    {
+        id: "Banca-2-llantas",
+        img: "/imgProducts/banca.avif",
+        name: "Banca",
+        materials: "Madera, Llantas y Metal",
+        price: "200.000",
+        description: "La banca combina madera, varillas metálicas y llantas de bicicleta recicladas. Es una pieza única y resistente, ideal para espacios al aire libre con un toque creativo."
+    },
+    {
+        id: "Perro-Metal",
+        img: "/imgProducts/perro.avif",
+        name: "Perro alambre",
+        materials: "Metal y Varillas",
+        price: "85.000",
+        description: "Cada detalle, desde las curvas suaves hasta los ángulos precisos, irradia vida y personalidad, convirtiéndose en una pieza de conversación destacada en cualquier espacio."
+    },
+    
+    {
+        id: "Mesa-4-Puestos",
+        img: "/imgProducts/mesa.avif",
+        name: "Mesa 4 Puestos",
+        materials: "Madera, Metal y discos bicicleta",
+        price: "350.000",
+        description: "Esta mesa es una obra de arte funcional que combina la calidez de la madera, la resistencia del metal y la creatividad de los discos para crear un mueble único."
+    },
+    {
+        id: "Silla-Reclinable",
+        img: "/imgProducts/silla.avif",
+        name: "Silla Reinclinable",
+        materials: "Madera y Metal",
+        price: "150.000",
+        description: "La estructura resistente y acolchado suave de la silla garantizan un descanso óptimo, mientras que su mecanismo ajustable permite adaptarse a las preferencias individuales.",
+        descriptionComplete: "lore "
+    },
+    
+   
+    {
+        id: "Caballo",
+        img: "/imgProducts/caballo.avif",
+        name: "Caballo",
+        materials: "Metal",
+        price: "80.000",
+        description: "La estructura resistente y acolchado suave de la silla garantizan un descanso óptimo, mientras que su mecanismo ajustable permite adaptarse a las preferencias individuales."
+    },
+]
--- src/lib/products.ts
+++ src/lib/products.ts
@@ -1,51 +1,27 @@
 export const PRODUCTS = [
     {
-        id: "Mesa-4-Puestos",
-        img: "/imgProducts/mesa.avif",
-        name: "Mesa 4 Puestos",
-        materials: "Madera, discos bicicleta",
-        price: "500.000",
-        description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf "
+        id: "Moto-cadena",
+        img: "/imgProducts/moto.avif",
+        name: "Moto",
+        materials: "Cadena de bicicleta",
+        price: "90.000",
+        description: "Este diseño único resalta tanto la creatividad como la funcionalidad, ofreciendo una alternativa innovadora en la construcción de vehículos."
     },
-    {
-        id: "Silla-Reclinable",
-        img: "/imgProducts/silla.avif",
-        name: "Silla Reinclinable",
-        materials: "Varilla, madera",
-        price: "500.000",
-        description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf ",
-        descriptionComplete: "lore "
-    },
+    
     {
         id: "Banca-2-llantas",
         img: "/imgProducts/banca.avif",
         name: "Banca",
-        materials: "Llantas, Madera",
-        price: "500.000",
-        description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf "
+        materials: "Madera, Llantas y Metal",
+        price: "200.000",
+        description: "La banca combina madera, varillas metálicas y llantas de bicicleta recicladas. Es una pieza única y resistente, ideal para espacios al aire libre con un toque creativo."
     },
     {
-        id: "Moto-cadena",
-        img: "/imgProducts/moto.avif",
-        name: "Moto",
-        materials: "Cadena de cicla",
-        price: "500.000",
-        description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf "
-    },
-    {
-        id: "Perro Alambre",
+        id: "Perro-Metal",
         img: "/imgProducts/perro.avif",
         name: "Perro alambre",
-        materials: "Metal, alambre",
-        price: "500.000",
-        description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf "
-    },
-    {
-        id: "Silla-Reclinable",
-        img: "/imgProducts/silla.avif",
-        name: "Silla Reinclinable",
-        materials: "Varilla, madera",
-        price: "500.000",
-        description: "Manualidad hecha a base de metal y de hierro que tgavskdahadjkdkj nakjd  dj dfkj fjdf jdfbjdfbfbf "
-    },
+        materials: "Metal y Varillas",
+        price: "85.000",
+        description: "Cada detalle, desde las curvas suaves hasta los ángulos precisos, irradia vida y personalidad, convirtiéndose en una pieza de conversación destacada en cualquier espacio."
+    }
 ]
