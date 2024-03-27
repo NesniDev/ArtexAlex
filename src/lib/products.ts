ec4f5d4c73b5405bdf47692709dab35fd83d5870
Nestnidev <neidernieto@unisangil.edu.co>
2024-03-27 10:20:01 -0500

    fix products and allproducts

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -23,5 +23,33 @@ export const PRODUCTS = [
         materials: "Metal y Varillas",
         price: "85.000",
         description: "Cada detalle, desde las curvas suaves hasta los ángulos precisos, irradia vida y personalidad, convirtiéndose en una pieza de conversación destacada en cualquier espacio."
-    }
+    },
+    
+    {
+        id: "Mesa-4-Puestos",
+        img: "/imgProducts/mesa.avif",
+        name: "Mesa 4 Puestos",
+        materials: "Madera, Metal y discos bicicleta",
+        price: "350.000",
+        description: "Esta mesa es una obra de arte funcional que combina la calidez de la madera, la resistencia del metal y la creatividad de los discos para crear un mueble único."
+    },
+    {
+        id: "Silla-Reclinable",
+        img: "/imgProducts/silla.avif",
+        name: "Silla Reinclinable",
+        materials: "Madera y Metal",
+        price: "150.000",
+        description: "La estructura resistente y acolchado suave de la silla garantizan un descanso óptimo, mientras que su mecanismo ajustable permite adaptarse a las preferencias individuales.",
+        descriptionComplete: "lore "
+    },
+    
+   
+    {
+        id: "Caballo",
+        img: "/imgProducts/caballo.avif",
+        name: "Caballo",
+        materials: "Metal",
+        price: "80.000",
+        description: "La estructura resistente y acolchado suave de la silla garantizan un descanso óptimo, mientras que su mecanismo ajustable permite adaptarse a las preferencias individuales."
+    },
 ]
