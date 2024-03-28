9de1e48f2207489ac080a8b47ae34d971de8fb84
Nestnidev <neidernieto@unisangil.edu.co>
2024-03-28 12:02:58 -0500

    fix ViewTransition -Script (is-inline|data-astro-rerun)

--- .netlify/functions-internal/ssr/chunks/AllProducts_CoycvwOC.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/AllProducts_CXK2Ey2H.mjs').then(n => n.A);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/_product__CpNMgZlO.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/_product__Dh9SEYcR.mjs');
-
-export { page };
