10e6d7593b88f5e5c439220c9c30f777f8324aff
Nestnidev <neidernieto@unisangil.edu.co>
2024-03-30 18:05:50 -0500

    fix styles of the products

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -5,7 +5,7 @@ export const PRODUCTS = [
         name: "Moto",
         materials: "Cadena de bicicleta",
         price: "90.000",
-        description: "Este diseño único resalta tanto la creatividad como la funcionalidad, ofreciendo una alternativa innovadora en la construcción de vehículos."
+        description: "Este diseño único resalta tanto la creatividad como la funcionalidad, ofreciendo una alternativa innovadora en la construcción de vehículos con cosas desechas."
     },
     {
         id: "Banca-2-llantas",
@@ -13,7 +13,7 @@ export const PRODUCTS = [
         name: "Banca",
         materials: "Madera, Llantas y Metal",
         price: "200.000",
-        description: "La banca combina madera, varillas metálicas y llantas de bicicleta recicladas. Es una pieza única y resistente, ideal para espacios al aire libre con un toque creativo."
+        description: "La banca combina madera, varillas metálicas y llantas de bicicleta recicladas. Es una pieza única y resistente, ideal para espacios al aire libre con un toque ."
     },
     {
         id: "Perro-Metal",
@@ -21,7 +21,7 @@ export const PRODUCTS = [
         name: "Perro alambre",
         materials: "Metal y Varillas",
         price: "85.000",
-        description: "Cada detalle, desde las curvas suaves hasta los ángulos precisos, irradia vida y personalidad, convirtiéndose en una pieza de conversación destacada en cualquier espacio."
+        description: "Cada detalle, desde las curvas suaves hasta los ángulos precisos, irradia vida y personalidad, convirtiéndose en una pieza destacada en cualquier ."
     }, 
     {
         id: "Mesa-4-Puestos",
