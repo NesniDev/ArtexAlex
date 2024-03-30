28b42e137fa26f452c7a3d31405c3e1d7d576cdf
Nestnidev <neidernieto@unisangil.edu.co>
2024-03-30 18:05:00 -0500

    Modify eslint and remove eslint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel(),
+  adapter: cloudflare()
 });
