5e0fcba2e7429dc62780e14464e171e30285584d
Nestnidev <neidernieto@unisangil.edu.co>
2024-04-03 16:42:34 -0500

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: cloudflare(),
-  site: "https://www.my-site.dev",
+  adapter: netlify(),
+  site: "https://www.my-site.dev"
 });
