da818f71520d6d2cc3c5051d3e6edc91e50e15a3
Nestnidev <neidernieto@unisangil.edu.co>
2024-04-08 19:23:15 -0500

    change a cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel(),
+  adapter: cloudflare(),
   site: "https://www.my-site.dev",
 });
