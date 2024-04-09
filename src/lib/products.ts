d859391e85e7d5da41a8c4aae5cf8b4a16311cc5
Nestnidev <neidernieto@unisangil.edu.co>
2024-04-09 13:50:06 -0500

    fix styles card of the products - Client petition

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -1,51 +1,44 @@
 export const PRODUCTS = [
     {
-        id: "Moto-cadena",
+        id: "Moto",
         img: "/imgProducts/moto.avif",
-        name: "Moto",
-        materials: "Cadena de bicicleta",
-        price: "90.000",
+        img2: "/imgProducts/moto.avif",
+        img3: "/imgProducts/moto.avif",
         description: "Este diseño único resalta tanto la creatividad como la funcionalidad, ofreciendo una alternativa innovadora en la construcción de vehículos con cosas desechas."
     },
     {
-        id: "Banca-2-llantas",
+        id: "Banca",
         img: "/imgProducts/banca.avif",
-        name: "Banca",
-        materials: "Madera, Llantas y Metal",
-        price: "200.000",
+        img2: "/imgProducts/moto.avif",
+        img3: "/imgProducts/moto.avif",
         description: "La banca combina madera, varillas metálicas y llantas de bicicleta recicladas. Es una pieza única y resistente, ideal para espacios al aire libre con un toque ."
     },
     {
-        id: "Perro-Metal",
+        id: "Perro",
         img: "/imgProducts/perro.avif",
-        name: "Perro alambre",
-        materials: "Metal y Varillas",
-        price: "85.000",
+        img2: "/imgProducts/moto.avif",
+        img3: "/imgProducts/moto.avif",
         description: "Cada detalle, desde las curvas suaves hasta los ángulos precisos, irradia vida y personalidad, convirtiéndose en una pieza destacada en cualquier ."
     }, 
     {
-        id: "Mesa-4-Puestos",
+        id: "Mesa",
         img: "/imgProducts/mesa.avif",
-        name: "Mesa 4 Puestos",
-        materials: "Madera, Metal y discos bicicleta",
-        price: "350.000",
+        img2: "/imgProducts/moto.avif",
+        img3: "/imgProducts/moto.avif",
         description: "Esta mesa es una obra de arte funcional que combina la calidez de la madera, la resistencia del metal y la creatividad de los discos para crear un mueble único."
     },
     {
-        id: "Silla-Reclinable",
+        id: "Silla",
         img: "/imgProducts/silla.avif",
-        name: "Silla Reinclinable",
-        materials: "Madera y Metal",
-        price: "150.000",
+        img2: "/imgProducts/moto.avif",
+        img3: "/imgProducts/moto.avif",
         description: "La estructura resistente y acolchado suave de la silla garantizan un descanso óptimo, mientras que su mecanismo ajustable permite adaptarse a las preferencias individuales.",
-        descriptionComplete: "lore "
     },
     {
         id: "Caballo",
         img: "/imgProducts/caballo.avif",
-        name: "Caballo",
-        materials: "Metal",
-        price: "80.000",
+        img2: "/imgProducts/moto.avif",
+        img3: "/imgProducts/moto.avif",
         description: "La estructura resistente y acolchado suave de la silla garantizan un descanso óptimo, mientras que su mecanismo ajustable permite adaptarse a las preferencias individuales."
     },
 ]
